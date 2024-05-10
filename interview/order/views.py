b218a1531afc3b8468e86cc952a714b1a6c2dcc7
joshuashevchuk1 <joshua.shevchuk.746@my.csun.edu>
2024-05-10 15:40:56 -0700

    Add on orders view and not the inventory views

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +12,26 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+from rest_framework import generics
+from django.utils import timezone
+from interview.order.models import Order
+from interview.order.serializers import OrderSerializer
+
+
+class OrderListBetweenDatesView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        # Get the start and embargo date from the query parameters
+        start_date = self.request.query_params.get('start_date')
+        embargo_date = self.request.query_params.get('embargo_date')
+
+        # Convert date strings to datetime objects
+        start_date = timezone.datetime.strptime(start_date, '%Y-%m-%d')
+        embargo_date = timezone.datetime.strptime(embargo_date, '%Y-%m-%d')
+
+        # Filter orders between the start and embargo date
+        queryset = Order.objects.filter(date__range=[start_date, embargo_date])
+        return queryset
